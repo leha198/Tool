08bf81437d2568a9440b5d47f3fb56e37a16098c
leha198 <41480901+leha198@users.noreply.github.com>
2020-09-21 22:19:53 +0700

    Update copy.sh

--- host/copy.sh
+++ host/copy.sh
@@ -1,49 +1,59 @@
-#!/bin/bash
+#!/bin/sh
 clear
-ls=$(ls -l -Ibkdata -Ilog|awk '/^d/ {print $9}')
+mkdir -p bkdata
+user=`ls -1 /home`
+ls=`ls -l -Ibkdata -Ilog|awk '/^d/ {print $9}'`
 PS3="Chose domain backup:"
 select domain in $ls; do
-	if [[ -d $domain ]]; then
+	if [ -f $domain/DocumentRoot/wp-config.php ]; then
+		cnf=$domain/DocumentRoot/wp-config.php
+		break
+	elif [ -f $domain/wp-config.php ]; then
+		cnf=$domain/wp-config.php
+		break
+	else
+		echo "Website not running wordpress..."
+		sleep 2; exit
+	fi
+done
+db_name=`grep 'DB_NAME' $cnf | awk -F"'" '{print $4}'`
+db_user=`grep 'DB_USER' $cnf | awk -F"'" '{print $4}'`
+db_pass=`grep 'DB_PASS' $cnf | awk -F"'" '{print $4}'`
+prefix=`grep '_prefix' $cnf | awk -F"'" '{print $2}'`
+mysqldump -u $db_user -p$db_pass $db_name > bkdata/$domain.sql
+if [ $? -eq 0 ] ; then
+	echo "Backup database $domain successful"
+else
+	echo "Backup database $domain fail"
+	sleep 2; exit
+fi
+cd $domain
+tar -czf /home/$user/bkdata/$domain.gz DocumentRoot --exclude DocumentRoot/wp-config.php
+if [ $? -eq 0 ]; then
+    echo "Backup code $domain successful"
+else
+    echo "Backup code $domain fail"
+fi
+cd
+wget -q script.lehait.net/host/restore.sh -O bkdata/restore.sh
+sed -i -e "s/insertdomain/$domain/g" -e "s/insertprefix/$prefix/g" bkdata/restore.sh
+function remote {
+	while read -p "Copy to server:" host; do
+		if [ ! -z $host ]; then
+			break
+		fi
+	done
+	while read -p "User SFTP:" account; do
+		if [ ! -z $account ]; then
+			break
+		fi
+	done
+}
+while remote; do
+	scp -r -P 9090 bkdata $account@$host:/home/$account
+	if [ $? -eq 0 ]; then
 		break
 	fi
 done
-rm -rf bkdata
-mkdir -p bkdata
-root=$domain/DocumentRoot
-if [[ -f $root/wp-config.php ]]; then
-	cg=$root/wp-config.php
-elif [[ -f $domain/wp-config.php ]]; then
-	cg=$domain/wp-config.php
-else
-	echo "Website not running Wordpress..."
-	sleep 2 && exit
-fi
-db_name=$(grep DB_NAME $cg |cut -d "'" -f 4)
-db_user=$(grep DB_USER $cg | cut -d "'" -f 4)
-db_pass=$(grep DB_PASSWORD $cg | cut -d "'" -f 4)
-table_prefix=$(grep '$table_prefix' $cg | cut -d "'" -f 2)
-mysqldump -u $db_user -p$db_pass $db_name > bkdata/$domain.sql
-if [ $? -eq 0 ] ; then
-	echo "Backup database successful"
-else
-	echo "Backup database fail"
-	sleep 2 && exit
-fi
-dir=$(pwd)
-cd $domain
-tar -cf $dir/bkdata/$domain.gz DocumentRoot --exclude DocumentRoot/wp-content/cache --exclude DocumentRoot/wp-config.php
-echo "Backup code successful" && cd
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-wget -q script.lehait.net/host/restore.sh -O bkdata/restore.sh
-sed -i -e "s/backupdomain/$domain/g" -e "s/backupprefix/$table_prefix/g" bkdata/restore.sh
-transfer(){
-	read -p "Another Host:" host
-	read -p "User SFTP:" acc
-	scp -r -P 9090 bkdata $acc@$host:/home/$acc/
-}
-transfer
-while [ $? -ge 1 ]; do
-	echo "Transfer fail..." && transfer
-done
-ssh -p 9090 $acc@$host "cd bkdata && sh restore.sh"
-rm -rf bkdata copy.sh
+ssh -p 9090 $account@$host "cd bkdata; sh restore.sh"
+rm -rf bkdata $0
