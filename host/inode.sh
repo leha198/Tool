d2f47e1dc997b42105edb2c8ad75a526b62c8891
leha198 <41480901+leha198@users.noreply.github.com>
2020-10-10 17:32:48 +0700

    Delete inode.sh

--- host/inode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-echo "Inode statistics for: $(pwd)"
-for d in `ls -1A`; do
-        c=$(find $d | wc -l)
-        echo -e "$c\t - $d"
-        done
-echo "Total: $(find $(pwd) | wc -l) file"
