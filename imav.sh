2716847db11c258c7c3558384d345b47e1b14914
leha198 <41480901+leha198@users.noreply.github.com>
2020-12-31 15:10:00 +0700

    Update imav.sh

--- imav.sh
+++ imav.sh
@@ -2,7 +2,6 @@
 systemctl stop imunify-antivirus
 sqlite3 /var/imunify360/imunify360.db <<EOF
 PRAGMA foreign_keys = ON;
-DELETE FROM malware_user_infected;
 DELETE FROM malware_scans where total_malicious = 0;
 EOF
 systemctl start imunify-antivirus
