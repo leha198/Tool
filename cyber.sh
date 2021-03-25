59294fafa533f85eb9d80cd15023afae2d2ef133
leha198 <41480901+leha198@users.noreply.github.com>
2021-03-25 21:50:56 +0700

    Update cyber.sh

--- cyber.sh
+++ cyber.sh
@@ -1,37 +1,34 @@
-#!/bin/bash
-#Install cyberpanel
-cyber(){
+#!/bin/sh
+function install_cyber {
 wget -O cyberpanel.sh https://cyberpanel.net/install.sh
 chmod +x cyberpanel.sh
 ./cyberpanel.sh -v ols -p r
 find /usr/local/lsws -type f -name "php.ini" | xargs sed -i -e 's/2M/1024M/g' -e 's/128M/256M/g' -e 's/8M/1024M/g'
 find /usr/local/lsws -type f -name "php.ini" | xargs sed -i -e 's/time = 30/time = 1000/g' -e 's/time = 60/time = 1000/g'
-sqlpw=`cat /etc/cyberpanel/mysqlPassword`
-mysql -u root -p$sqlpw <<EOF	
+pass=`cat /etc/cyberpanel/mysqlPassword`
+mysql -u root -p$pass <<EOF	
 use cyberpanel;	
 update packages_package set diskSpace = '0' where id = '1';	
 update packages_package set bandwidth = '0' where id = '1';	
 update packages_package set allowedDomains = '0' where id = '1';	
 EOF
 }
-#Check OS and install
-install(){
+function install {
 if [ -f /etc/redhat-release ]; then
+	echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+	sysctl -p
 	yum update -y
-	cyber
+	install_cyber
 	echo "max_allowed_packet=1024M" >> /etc/my.cnf
 elif [ -f /etc/lsb-release ]; then
-	apt-get update
-	sleep 3
-	apt-get upgrade -y
-	cyber
+	apt update; apt upgrade -y
+	install_cyber
 	echo "max_allowed_packet=1024M" >> /etc/mysql/my.cnf
 fi
 rm -rf cyberpanel*
 }
-#Check install cyberpanel
 if [ -d /usr/local/CyberCP ]; then
-	cyberpanel upgrade
+	curl https://raw.githubusercontent.com/usmannasir/cyberpanel/stable/preUpgrade.sh | sh
 else
-	sleep 2 && install
+	install
 fi
