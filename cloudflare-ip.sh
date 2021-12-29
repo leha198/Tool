4d906aefad0f178b02466cf79c8f0509589e9223
leha198 <41480901+leha198@users.noreply.github.com>
2021-12-29 21:42:00 +0700

    Update cloudflare-ip.sh

--- cloudflare-ip.sh
+++ cloudflare-ip.sh
@@ -2,9 +2,9 @@
 cf_ips() {
   for type in v4 v6; do
     echo "# IP$type"
-    curl -s "https://www.cloudflare.com/ips-$type" | sed "s|^|allow |g" | sed "s|\$|;|g"
+    curl -s "https://www.cloudflare.com/ips-$type" | sed "s|^|set_real_ip_from |g" | sed "s|\$|;|g"
     echo
   done
   echo "# Generated at $(LC_ALL=C date)"
 }
-(cf_ips && echo "deny all;") > allow-cloudflare-only.conf
+(cf_ips && echo "real_ip_header X-Forwarded-For;") > cloudflare.conf
