b7693e4402f72127f11d8780a8975f3e0411dbcc
leha198 <41480901+leha198@users.noreply.github.com>
2022-03-17 14:04:57 +0700

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -4,5 +4,5 @@ mail2=
 pw1=
 pw2=
 for user in $(cat users.txt); do
-imapsync --host1 $mail1 --user1 $user --password1 $pw1 --ssl1 --host2 $mail2 --user2 $user --password2 $pw2 --ssl2 --syncinternaldates --usecache --useheader "Message-Id" --delete2duplicates
+imapsync --host1 $mail1 --user1 $user --password1 $pw1 --ssl1 --host2 $mail2 --user2 $user --password2 $pw2 --ssl2 --syncinternaldates --usecache --useheader "Message-Id"
 done
