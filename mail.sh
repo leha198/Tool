086f0faa9dbd553c587f0d92ab38c6a226181463
leha198 <41480901+leha198@users.noreply.github.com>
2022-08-23 15:13:38 +0700

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 mail1=
 mail2=
-pw1=
-pw2=
+pw=
 for user in $(cat users.txt); do
-imapsync --host1 $mail1 --user1 $user --password1 $pw1 --ssl1 --host2 $mail2 --user2 $user --password2 $pw2 --ssl2 --syncinternaldates --usecache --useheader "Message-Id"
+    imapsync --host1 $mail1 --user1 $user --password1 $pw --ssl1 --host2 $mail2 --user2 $user --password2 $pw --ssl2 --syncinternaldates --usecache --useheader "Message-Id"
 done
