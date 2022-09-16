5da318c157aecfbc680ccaf836e6d9660615c0a2
leha198 <41480901+leha198@users.noreply.github.com>
2022-09-16 18:01:13 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,37 +1,24 @@
-#!/bin/sh
-cd
-user=`ls -1 /home`
-domain=insertdomain
-prefix=insertprefix
-ls=`ls -l -Ilog -Ibkdata | awk '/^d/ {print $9}'`
+#!/bin/bash
+usr="/home/$(whoami)"
+src_url="failoverhosting.com.vn"
+bk=domain_ins
 PS3="Chose domain restore:"
-select restore in $ls; do
-	if [ -f $restore/wp-config.php ]; then
-		cnf=$restore/wp-config.php
-		break
-	else
-		echo "Please delete website and create website with wordpress"
-		exit 2
-	fi
-done
-rm -rf $restore/DocumentRoot
-tar -xzf /home/$user/bkdata/$domain.gz --directory $restore
-db_name=`grep 'DB_NAME' $cnf | awk -F"'" '{print $4}'`
-db_user=`grep 'DB_USER' $cnf | awk -F"'" '{print $4}'`
-db_pass=`grep 'DB_PASS' $cnf | awk -F"'" '{print $4}'`
-db_prefix=`grep '_prefix' $cnf | awk -F"'" '{print $2}'`
-mysql -u $db_user -p$db_pass -e "drop database if exists $db_name;"
-mysql -u $db_user -p$db_pass -e "create database $db_name;"
-mysql -u $db_user -p$db_pass $db_name < /home/$user/bkdata/$domain.sql
-if [ $? -eq 0 ] ; then
-	echo "Clone to $restore successful"
-else
-	echo "Clone to $restore fail"
-fi
-mysql -u $db_user -p$db_pass << EOF
-use $db_name;
-update ${prefix}options set option_value = 'http://$restore' where option_id = 1;
-update ${prefix}options set option_value = 'http://$restore' where option_id = 2;
-EOF
-sed -i "s/$db_prefix/$prefix/g" $cnf
-rm -rf bkdata; exit
+select dm in `ls -l -Ilog -I${domain} | awk '/^d/ {print $9}'`; do
+	if [ -d ${dm}/DocumentRoot/wp-content ]; then
+            break
+        else
+            echo "Script only support WordPress..."
+            exit 2
+        fi
+    done
+
+	cd ${usr}/${dm}/DocumentRoot
+	admin_pw=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 12`	
+    wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O wp
+	php74 wp core install --url=${dm} --title=Blog --admin_user=admin --admin_password=${admin_pw} --admin_email=tenten@gmail.com 2>&1 >/dev/null
+	php74 wp option update home "http://${dm}" > /dev/null
+	php74 wp option update siteurl "http://${dm}" > /dev/null
+	plugin_act
+	mv -f ${usr}/${domain}.wpress wp-content/ai1wm-backups/${domain}.wpress
+	php74 wp ai1wm restore ${domain}.wpress --yes > /dev/null
+	plugin_deact
