3f562d161ba35cdaa2773850f6eea9ecbd7edf35
leha198 <41480901+leha198@users.noreply.github.com>
2022-09-19 10:41:03 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,86 +1,98 @@
-#!/bin/sh
-clear
-mkdir -p bkdata
-user=`ls -1 /home`
-function check {
-    ls=`ls -l -Ibkdata -Ilog | awk '/^d/ {print $9}'`
-    PS3="Chose domain backup:"
-    select domain in $ls; do
-        if [ -f $domain/DocumentRoot/wp-config.php ]; then
-            cnf=$domain/DocumentRoot/wp-config.php
-            break
-        elif [ -f $domain/wp-config.php ]; then
-            cnf=$domain/wp-config.php
+#!/bin/bash
+usr="/home/$(whoami)"
+src_url="failoverhosting.com.vn"
+
+function plugin_act {
+    wget -q ${src_url}/all-in-one-wp-migration.zip -O all-in-one-wp-migration.zip
+    wget -q ${src_url}/all-in-one-wp-migration-unlimited-extension.zip -O all-in-one-wp-migration-unlimited-extension.zip
+    php74 wp plugin install all-in-one-wp-migration.zip --activate > /dev/null
+    php74 wp plugin install all-in-one-wp-migration-unlimited-extension.zip --activate > /dev/null
+}
+
+function plugin_deact {
+    php74 wp plugin deactivate all-in-one-wp-migration all-in-one-wp-migration-unlimited-extension > /dev/null
+    php74 wp plugin delete all-in-one-wp-migration all-in-one-wp-migration-unlimited-extension > /dev/null
+    rm -rf wp all-in-one-wp-*.zip wp-content/ai1wm-backups
+}
+
+function backup {
+	PS3="Chose domain backup:"
+	select domain in `ls -l -Ilog | awk '/^d/ {print $9}'`; do
+		if [ -d ${domain}/DocumentRoot/wp-content ]; then
+			break
+		else
+			echo "Script only support WordPress..."
+			exit 2
+		fi
+	done
+
+	cd ${usr}/${domain}/DocumentRoot
+	wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O wp
+	plugin_act
+	bk_file=`php74 wp ai1wm backup | grep "location" | awk '{print $3}'`
+	mv -f ${bk_file} ${usr}/${domain}.wpress
+	plugin_deact
+}
+
+function restore {
+	cd ${usr}
+    PS3="Chose domain restore:"
+    select dm in `ls -l -Ilog -I${domain} | awk '/^d/ {print $9}'`; do
+        if [ -d ${dm}/DocumentRoot/wp-content ]; then
             break
         else
-            echo "Website not running wordpress..."
+            echo "Script only support WordPress..."
             exit 2
         fi
     done
+
+	cd ${usr}/${dm}/DocumentRoot
+	admin_pw=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 12`	
+    wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O wp
+	php74 wp core install --url=${dm} --title=Blog --admin_user=admin --admin_password=${admin_pw} --admin_email=tenten@gmail.com 2>&1 >/dev/null
+	php74 wp option update home "http://${dm}" > /dev/null
+	php74 wp option update siteurl "http://${dm}" > /dev/null
+	plugin_act
+	mv -f ${usr}/${domain}.wpress wp-content/ai1wm-backups/${domain}.wpress
+	php74 wp ai1wm restore ${domain}.wpress --yes > /dev/null
+	plugin_deact
 }
-function getdb {
-    db_name=`grep 'DB_NAME' $cnf | awk -F"'" '{print $4}'`
-    db_user=`grep 'DB_USER' $cnf | awk -F"'" '{print $4}'`
-    db_pass=`grep 'DB_PASS' $cnf | awk -F"'" '{print $4}'`
-}
-function bkdb {
-    getdb
-    prefix=`grep '_prefix' $cnf | awk -F"'" '{print $2}'`
-    mysqldump -u $db_user -p$db_pass $db_name > bkdata/$domain.sql
-    if [ $? -eq 0 ]; then
-        echo "Backup database successful"
-    else
-        echo "Backup database fail"
-        exit 2
-    fi
-}
-function bkcode {
-    cd $domain
-    tar -czf /home/$user/bkdata/$domain.gz DocumentRoot --exclude DocumentRoot/wp-config.php
-    if [ $? -eq 0 ]; then
-        echo "Backup code $domain successful"
-    else
-        echo "Backup code $domain fail"
-    fi
-    cd
-}
-function clone {
-    ls=`ls -l -Ibkdata -Ilog -I$domain | awk '/^d/ {print $9}'`
-    PS3="Choose domain restore:"
-    select restore in $ls; do
-        if [ -f $restore/wp-config.php ]; then
-            cnf=$restore/wp-config.php
+
+function sftp {
+    while read -p "Copy to server:" host; do
+        if [ ! -z ${host} ]; then
             break
-        else
-            echo "Please delete website and create website with wordpress"
         fi
     done
-    getdb
-    mysql -u $db_user -p$db_pass -e "drop database if exists $db_name;"
-    mysql -u $db_user -p$db_pass -e "create database $db_name;"
-    mysql -u $db_user -p$db_pass $db_name < bkdata/$domain.sql
-    if [ $? -eq 0 ]; then
-        echo "Restore database successful"
-    else
-        echo "Restore database fail"
-        exit 2
-    fi
-    mysql -u $db_user -p$db_pass << EOF
-use $db_name;
-update ${prefix}options set option_value = 'http://$restore' where option_id = 1;
-update ${prefix}options set option_value = 'http://$restore' where option_id = 2;
-EOF
-    cp -r $domain/DocumentRoot $restore > /dev/null 2>&1
-    sed -i "s/wp_/$prefix/g" $cnf
-    rm -rf bkdata
+    while read -p "User SFTP:" acc; do
+        if [ ! -z ${acc} ]; then
+            break
+        fi
+    done
 }
+
+function remote {
+	cd ${usr}
+	wget -q https://raw.githubusercontent.com/leha198/script/master/restore.sh -O restore.sh
+	sed -i "s|domain_ins|${domain}|g" restore.sh
+    while sftp; do
+        scp -P 9090 ${domain}.wpress restore.sh ${acc}@${host}:/home/${acc}
+        if [ $? -eq 0 ]; then
+            break
+        fi
+    done
+    ssh -p 9090 ${acc}@${host} "sh restore.sh; rm -f restore.sh"
+    rm -f ${domain}.wpress restore.sh
+}
+
 function menu {
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
-    echo "1. Backup website"
-    echo "2. Clone website"
+    echo "=================================="
+    echo "1. Clone website"
+    echo "2. Copy website to another server"
     echo "3. Exit"
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "=================================="
 }
+
 function pause {
     read -p "Press [Enter] key to menu..."
 }
@@ -88,21 +100,14 @@ function pause {
 function option {
     read -p "Please enter choice:" choice
     case $choice in
-        1 ) check
-            bkdb
-            bkcode
-            pause
-            ;;
-        2 ) 
-            check
-            bkdb
-            clone
-            pause
-            ;;
-        3 ) 
-            rm -f $0
+        1 ) backup
+            restore
             exit
-            ;;
+	    ;;
+        2 ) backup
+            remote
+            exit
+	    ;;
         * ) echo "Error..."; sleep 1
     esac
 }
