36d27c939d00d23f373db2ffa6b02ac6b1b0751e
leha198 <41480901+leha198@users.noreply.github.com>
2023-06-15 09:18:39 +0700

    Update enable_tls_win7.cmd

--- enable_tls_win7.cmd
+++ enable_tls_win7.cmd
@@ -15,7 +15,7 @@ if "%arch%"=="64" (
 )
 
 REM Download the file using the appropriate URL
-echo Update Hotfix and Enable TLS...
+echo Update Hotfix and enable TLS 1.2...
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', 'kb3140245.msu')"
 
 REM Update hostfix
