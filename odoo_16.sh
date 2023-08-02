713fec165d4cf26380baecd96465abb31f12ecc1
Umemura Kanzaki <85058029+dominhtan@users.noreply.github.com>
2023-08-02 12:12:52 +0700

    Update odoo_16.sh

--- odoo_16.sh
+++ odoo_16.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-#Setup Odoo version 16 with Ubuntu 22.2
-apt update -y
-sudo adduser --system --quiet --shell=/bin/bash --home=/opt/odoo --gecos 'odoo' --group odoo
-sudo mkdir /etc/odoo && mkdir /var/log/odoo/
-apt-get update && sudo apt-get install postgresql postgresql-server-dev-all python3-pil python3-lxml python3-dev python3-pip python3-setuptoolsnpm nodejs git gdebi libldap2-dev libsasl2-dev libxml2-dev libxslt1-dev libjpeg-dev python3-pip python3 zlib1g-dev build-essential libssl-dev libffi-dev libmysqlclient-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev gdebi-core -y
+
+# Add system user for Odoo
+adduser --system --quiet --shell=/bin/bash --home=/opt/odoo --gecos 'odoo' --group odoo
+
+# Create necessary directories
+mkdir -p /etc/odoo /var/log/odoo/
+
+# Update package lists
+apt update
+
+# Install required dependencies
+apt install -y python3-pil python3-lxml python3-dev python3-pip \
+python3-setuptools npm nodejs git gdebi libldap2-dev libsasl2-dev \
+libxml2-dev libxslt1-dev libjpeg-dev python3-pip python3 zlib1g-dev \
+build-essential libssl-dev libffi-dev libmysqlclient-dev libpq-dev \
+libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev gdebi-core
+
+# Install PostgreSQL and enable as a service
+apt install postgresql postgresql-server-dev-all -y
+systemctl enable --now postgresql
+
+# Upgrade Python packages
 python3 -m pip install wheel setuptools pip --upgrade
+
+# Restart PostgreSQL service
 sudo service postgresql restart
+
 git clone --depth=1 --branch=16.0 https://github.com/odoo/odoo.git /opt/odoo/odoo
 sudo chown odoo:odoo /opt/odoo/ -R && sudo chown odoo:odoo /var/log/odoo/ -R && cd /opt/odoo/odoo && sudo pip3 install -r requirements.txt
 sudo npm install -g less less-plugin-clean-css rtlcss -y
